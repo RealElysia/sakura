41078434336dc8415d07fff5c542c57df9426d84
ayaka <kxg38479643@163.com>
2024-01-22 14:45:06 +0800

    更新抽卡小工具方法

--- blog/src/main/java/com/elysia/controller/Genshin.java
+++ blog/src/main/java/com/elysia/controller/Genshin.java
@@ -1,91 +1,162 @@
 package com.elysia.controller;
 
 import com.elysia.services.UserServices;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.elysia.entity.*;
 import com.alibaba.fastjson.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.HashMap;
+
 @Controller
 @RequestMapping("/")
 public class Genshin {
-    int count;
+    int oneCount=0;
+    int tenCount=0;
+    int total=0;
     String result;
     @Autowired
     UserServices us;
 
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
-    @PostMapping("/cardOne")
+    // 单次抽卡
+    @PostMapping("/getOneRoles")
     @ResponseBody
-    public String Once(@RequestParam String role, int yuan){
-        String str = us.getJson();
-        JSONObject c = JSON.parseObject(str);
-        HashMap<String, Object> res = new HashMap<>();
-        if (role.equals("ayaka") && yuan>=160){
-            logger.info("抽取卡池为:" + c.getJSONArray("roles_1"));
-            Random rd = new Random();
-            count = rd.nextInt(10);
-            result = c.getJSONArray("roles_1").get(count).toString();
-            yuan -= 160;
-            res.put("success", true);
-            res.put("card", result);
-            res.put("yuan", yuan);
-        }else if (role.equals("yomiya") && yuan>=160) {
-            logger.info("抽取卡池为:" + c.getJSONArray("roles_2"));
-            Random rd = new Random();
-            count = rd.nextInt(10);
-            result = c.getJSONArray("roles_2").get(count).toString();
-            yuan -= 160;
-            res.put("success", true);
-            res.put("card", result);
-            res.put("yuan", yuan);
-        }else if (role.equals("keqing") && yuan>=160) {
-            logger.info("抽取卡池为:" + c.getJSONArray("roles_3"));
-            Random rd = new Random();
-            count = rd.nextInt(10);
-            result = c.getJSONArray("roles_3").get(count).toString();
-            yuan -= 160;
-            res.put("success", true);
-            res.put("card", result);
-            res.put("yuan", yuan);
+    public String oneRoles(@RequestParam String role, int yuan){
+        // 存储当前抽卡结果
+        HashMap<String, Object> resp = new HashMap<>();
+        resp.put("code", 0);
+        resp.put("success", true);
+
+        if (yuan >= 160){
+           HashMap<String, Object> r = oneRoleCards(role, yuan);
+           resp.put("role", r.get("role"));
+           resp.put("total", r.get("total"));
+           resp.put("yuan", r.get("yuan"));
+           return JSON.toJSONString(resp);
         }
-        return JSON.toJSONString(res);
+        return JSON.toJSONString(resp);
     }
-    @PostMapping("/cardTen")
+
+    // 十次抽卡
+    @PostMapping("/getTenRoles")
     @ResponseBody
-    public String cardTen(@RequestParam String role, int yuan){
-        String str = us.getJson();
-        JSONObject c = JSON.parseObject(str);
-        HashMap<String, Object> res = new HashMap<>();
-        if (role.equals("ayaka") && yuan>=1600){
-            logger.info("卡池" + c.getJSONArray("roles_1"));
-            result = c.getJSONArray("roles_1").toString();
-            yuan -= 1600;
-            res.put("card", result);
-            res.put("success", true);
-            res.put("yuan", yuan);
-        } else if (role.equals("yomiya") && yuan>=1600){
-            logger.info("卡池" + c.getJSONArray("roles_2"));
-            result = c.getJSONArray("roles_2").toString();
-            yuan -= 1600;
-            res.put("card", result);
-            res.put("success", true);
-            res.put("yuan", yuan);
-        }else if (role.equals("keqing") && yuan>=1600){
-            logger.info("卡池" + c.getJSONArray("roles_3"));
-            result = c.getJSONArray("roles_3").toString();
-            yuan -= 1600;
-            res.put("card", result);
-            res.put("success", true);
-            res.put("yuan", yuan);
-        }else {
-            return "{\"success\":true, \"MsgNotFound\": \"出错了，请联系管理员吧！\"}";
+    public String tenRoles(@RequestParam String role, int yuan){
+        // 存储当前抽卡结果
+        HashMap<String, Object> resp = new HashMap<>();
+        resp.put("code", 0);
+        resp.put("success", true);
+
+        if (yuan >= 1600) {
+            HashMap<String, Object> r = tenRoleCards(role, yuan);
+            resp.put("roles", r.get("roles"));
+            resp.put("total", r.get("total"));
+            resp.put("yuan", r.get("yuan"));
+            return JSON.toJSONString(resp);
         }
-        return JSON.toJSONString(res);
+        return JSON.toJSONString(resp);
+    }
+
+    // 单次
+    public HashMap<String, Object> oneRoleCards(String role, int yuan){
+        oneCount++;
+        tenCount++;
+        total++;
+        yuan -= 160;
+        // 取出角色信息枚举值
+        ArrayList<Object> array = new ArrayList<>();
+        HashMap<String, Object> hash = new HashMap<>();
+        // 随机数抽取
+        Random ran = new Random();
+        // 角色选择
+        switch (role) {
+            case "ayaka" -> array.addAll(Arrays.asList(Ayaka.values()));
+            case "yomiya" -> array.addAll(Arrays.asList(Yomiya.values()));
+            case "keqing" -> array.addAll(Arrays.asList(KeQing.values()));
+        }
+
+        int d = ran.nextInt(array.size());
+        String res = array.get(d).toString();
+        String bd = array.get(0).toString();  // 金色大保底
+        // 保底判断
+        if (oneCount==10){
+            oneCount = 0;
+            int one = ran.nextInt(0, 11);
+            String str = array.get(one).toString();
+            hash.put("role", str);
+            hash.put("total", total);
+            hash.put("yuan", yuan);
+            if (str.equals(bd)){
+                tenCount = 0;
+            }
+            return hash;
+        } else if (oneCount == 80) {
+            hash.put("role", bd);
+            hash.put("total", 80);
+            hash.put("yuan", yuan);
+            return hash;
+        } else if (res.equals(bd)) {
+            hash.put("role", bd);
+            hash.put("total", total);
+            hash.put("yuan", yuan);
+            tenCount = 0;
+            oneCount = 0;
+            return hash;
+        }
+        hash.put("role", res);
+        hash.put("total", total);
+        hash.put("yuan", yuan);
+        return hash;
+    }
+    // 十连抽
+    public HashMap<String, Object> tenRoleCards(String role, int yuan){
+        tenCount += 10;
+        total += 10;
+        yuan -= 1600;
+        // 取出角色信息枚举值
+        ArrayList<Object> array = new ArrayList<>();
+        ArrayList<Object> roles = new ArrayList<>();
+
+        HashMap<String, Object> hash = new HashMap<>();
+        hash.put("roles", roles);
+        // 随机数抽取
+        Random ran = new Random();
+        // 角色选择
+        switch (role) {
+            case "ayaka" -> array.addAll(Arrays.asList(Ayaka.values()));
+            case "yomiya" -> array.addAll(Arrays.asList(Yomiya.values()));
+            case "keqing" -> array.addAll(Arrays.asList(KeQing.values()));
+        }
+        if (tenCount==80){
+            String a = array.get(0).toString();
+            roles.add(a);
+            for (int i=0; i<9; i++){
+                int b = ran.nextInt(22);
+                roles.add(array.get(b).toString());
+            }
+            tenCount = 0;
+            hash.put("total", total);
+            hash.put("yuan", yuan);
+            return hash;
+        }
+        // 正常操作
+        for (int i=0; i<10; i++){
+            int b = ran.nextInt(22);
+            roles.add(array.get(b).toString());
+            String lh = array.get(0).toString();
+            if (role.contains(lh)){
+                tenCount = 0;
+            }
+        }
+        hash.put("total", total);
+        hash.put("yuan", yuan);
+        return hash;
     }
 
     // 充值
--- blog/src/main/java/com/elysia/controller/playController.java
+++ blog/src/main/java/com/elysia/controller/playController.java
@@ -4,14 +4,16 @@ import com.elysia.entity.player;
 import com.elysia.services.playServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Controller
+@RequestMapping("/")
 public class playController {
+    @Autowired
     private playServices ps;
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -23,6 +25,7 @@ public class playController {
     public String login(){
         return "login";
     }
+
     // 登陆页
     @ResponseBody
     @PostMapping("/loginIn")
@@ -37,6 +40,7 @@ public class playController {
             return new player(404, "error", "MsgNotFound");
         }
     }
+
     // 首页博客
     @RequestMapping("/home")
     public String Home(){
